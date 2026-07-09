f36fe22d845585b0169a24f74779c3633f30573c
Rajesh <rajesh.techsrp@gmail.com>
2026-07-09 20:08:04 +0530

    updated today

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,5 @@
-fun main(){
-
-    println("Hello Welcome to Kotlin")
+fun main() {
+    println("Welcome to my first app!")
+    println("I am learning Kotlin")
+    println("This is going to be fun!")
 }
